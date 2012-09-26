8f315ef3c38ac9a007eb7a9c86c80c559e8e8a70
Steve Kondik <shade@chemlab.org>
2012-09-25 23:35:40 -0700

    qcom-common: Only build for Samsung devices
    Change-Id: Idfabf6d4bc9e1b8ce2c01be049c7a608bbd4ab66

--- Android.mk
+++ Android.mk
@@ -22,7 +22,11 @@
 
 LOCAL_PATH := $(call my-dir)
 
+ifeq ($(BOARD_VENDOR),samsung)
+
 # TODO: Add 8660 when refactored
 ifeq ($(call is-board-platform-in-list,msm8960),true)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
+endif
+
