5b887be6bcacb2c8cda413b19366d23a5b8d49e6
Steve Kondik <shade@chemlab.org>
2012-11-18 18:44:08 -0800

    qcom-common: Define TARGET_ARCH
     * Required in MR1
    
    Change-Id: I058fb16d6a801a7b329af9c095259999762e2055

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -18,6 +18,7 @@ BOARD_VENDOR := samsung
 TARGET_NO_BOOTLOADER := true
 
 # Architecture
+TARGET_ARCH := arm
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
