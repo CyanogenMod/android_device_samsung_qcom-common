fda18f076f7583e64c32c81e26f7840a82e3a0e7
Steve Kondik <shade@chemlab.org>
2013-02-06 21:34:29 -0800

    qcom-common: Don't set ARCH_VARIANT here
     * Set it in the SoC specific configuration since this is shared now.
    
    Change-Id: Ied597e36d8c246c15871e8480eb33294c74e8bba

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -22,7 +22,6 @@ TARGET_ARCH := arm
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_ARCH_VARIANT_CPU := cortex-a9
 
 # Enable WEBGL in WebKit
 ENABLE_WEBGL := true
