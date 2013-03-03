fa80d522f0054efadfea08e4de20476a6432ebf8
Daniel Drown <dan-android@drown.org>
2013-03-03 13:42:35 -0600

    only turn off accept_ra_defrtr for rmnet interfaces
    accept_ra_defrtr=0 on wifi interfaces breaks IPv6 for networks using RA.  For
    wifi networks, the kernel sets up the default route as well as the interface
    address (in other words: with defrtr=0, wifi interfaces get an address but no
    default route).  This is not the case for the rmnet (ril) interfaces, as
    Samsung's ril libraries configure the default IPv6 route after setting up the
    PDP.

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -65,7 +65,7 @@ baseband=`getprop ro.baseband`
 # Suppress default route installation during RA for IPV6; user space will take
 # care of this
 #
-for file in /proc/sys/net/ipv6/conf/*
+for file in /proc/sys/net/ipv6/conf/rmnet*
 do
   echo 0 > $file/accept_ra_defrtr
 done
