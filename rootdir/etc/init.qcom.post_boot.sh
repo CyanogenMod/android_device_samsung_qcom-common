cc3c141e4ef0615a53d880ea64771d97708948ce
Steve Kondik <shade@chemlab.org>
2013-03-12 10:16:07 -0700

    qcom-common: Enable Krait retention
     * Enable new Krait retention mode on 3.4 kernel
     * Also online all CPUs at boot
    
    Change-Id: I204a36734796cb2b6d1c465b03b7637c4d25948b

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -93,6 +93,10 @@ case "$target" in
          echo 1 > /sys/module/rpm_resources/enable_low_power/pxo
          echo 1 > /sys/module/rpm_resources/enable_low_power/vdd_dig
          echo 1 > /sys/module/rpm_resources/enable_low_power/vdd_mem
+         echo 1 > /sys/module/pm_8x60/modes/cpu0/retention/idle_enabled
+         echo 1 > /sys/module/pm_8x60/modes/cpu1/retention/idle_enabled
+         echo 1 > /sys/module/pm_8x60/modes/cpu2/retention/idle_enabled
+         echo 1 > /sys/module/pm_8x60/modes/cpu3/retention/idle_enabled
          echo 1 > /sys/module/pm_8x60/modes/cpu0/power_collapse/suspend_enabled
          echo 1 > /sys/module/pm_8x60/modes/cpu1/power_collapse/suspend_enabled
          echo 1 > /sys/module/pm_8x60/modes/cpu2/power_collapse/suspend_enabled
@@ -106,6 +110,9 @@ case "$target" in
          echo 1 > /sys/module/pm_8x60/modes/cpu2/standalone_power_collapse/idle_enabled
          echo 1 > /sys/module/pm_8x60/modes/cpu3/standalone_power_collapse/idle_enabled
          echo 1 > /sys/module/pm_8x60/modes/cpu0/power_collapse/idle_enabled
+         echo 1 > /sys/devices/system/cpu/cpu1/online
+         echo 1 > /sys/devices/system/cpu/cpu2/online
+         echo 1 > /sys/devices/system/cpu/cpu3/online
          echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
          echo "interactive" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
          echo "interactive" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
