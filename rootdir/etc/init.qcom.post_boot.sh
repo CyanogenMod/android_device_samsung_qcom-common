fe3229b1b04c990bebdff289cb62b62d64b75036
Dilip Gudlur <dgudlur@codeaurora.org>
2013-05-21 17:39:23 +0000

    qcom-common: ondemand: Modify Sampling Down Factor to 2
    Change needed to help improve frequency scaling
    in ondemand governor. Sampling Down Factor
    is set to 2 in the init.qcom.post_boot.sh
    
    Change-Id: I1f8eb4fabce24ed7076251f2cc1ac96e8dc79dd4

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -298,7 +298,7 @@ case "$target" in
         echo 50000 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
         echo 90 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
         echo 1 > /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
-        echo 4 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
+        echo 2 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
         echo 10 > /sys/devices/system/cpu/cpufreq/ondemand/down_differential
         echo 300000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
         echo 300000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
