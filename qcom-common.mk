239e6250fcf6e4678312dcde4935ffbff35f0be9
Steve Kondik <shade@chemlab.org>
2013-05-31 01:35:26 -0700

    qcom-common: Add RIL startup script for IPv6 tethering
    Change-Id: I6c1fba6ea51451b218c5daae30ae0dc37838f7e0

--- qcom-common.mk
+++ qcom-common.mk
@@ -81,7 +81,8 @@ PRODUCT_PACKAGES += \
     lpm.rc \
     init.qcom.lpm_boot.sh \
     init.qcom.thermal_conf.sh \
-    init.recovery.qcom.rc
+    init.recovery.qcom.rc \
+    init.qcom.ril.sh
 
 # Charger
 PRODUCT_PACKAGES += charger charger_res_images
