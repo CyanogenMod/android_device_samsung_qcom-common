1890bcf708793b3c122c40afa83c4cdf27cc715e
Shareef Ali <shareefalis@cyanogenmod.org>
2013-07-25 20:23:17 -0500

    qcom-common: fix copy error
    Change-Id: I7bb24cdea8863d8702740da81937e2d859871530

--- qcom-common.mk
+++ qcom-common.mk
@@ -63,7 +63,6 @@ PRODUCT_PACKAGES += \
 # for bugmailer
 PRODUCT_PACKAGES += send_bug
 PRODUCT_COPY_FILES += \
-    system/extras/bugmailer/bugmailer.sh:system/bin/bugmailer.sh \
     system/extras/bugmailer/send_bug:system/bin/send_bug
 
 # Thermal profiles
