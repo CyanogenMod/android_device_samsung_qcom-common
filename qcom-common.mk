913397a3241a020bc7494545186f64223d05ffe1
David Ferguson <ferguson.david@gmail.com>
2013-08-07 15:12:51 -0400

    qcom-common: show LTE icon instead of 4G
      * This is to mimic the behavior of the Google Play Edition phones.
    
    Change-Id: I91847194b7e98be6ffac5fbbb3ce4706cfeb8ccc

--- qcom-common.mk
+++ qcom-common.mk
@@ -99,3 +99,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += charger charger_res_images
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+# Common overlay
+DEVICE_PACKAGE_OVERLAYS += device/samsung/qcom-common/overlay
