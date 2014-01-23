a0bf84bab477130bbd25e5e41e9c73dad0b63c0e
Shareef Ali <shareefalis@cyanogenmod.org>
2014-01-23 11:08:14 -0600

    qcom-common: revert retention changes
    * causes reboots
    
    Change-Id: Iab4d9adf5051ef430874eb3709239811cded28db

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -310,11 +310,7 @@ case "$target" in
         echo 1 > /sys/module/msm_pm/modes/cpu1/retention/idle_enabled
         echo 1 > /sys/module/msm_pm/modes/cpu2/retention/idle_enabled
         echo 1 > /sys/module/msm_pm/modes/cpu3/retention/idle_enabled
-        echo 1 > /sys/module/msm_pm/modes/cpu0/retention/suspend_enabled
-        echo 1 > /sys/module/msm_pm/modes/cpu1/retention/suspend_enabled
-        echo 1 > /sys/module/msm_pm/modes/cpu2/retention/suspend_enabled
-        echo 1 > /sys/module/msm_pm/modes/cpu3/retention/suspend_enabled
-        echo 1 > /sys/module/msm_thermal/core_control/enabled
+        echo 0 > /sys/module/msm_thermal/core_control/enabled
         echo 1 > /sys/devices/system/cpu/cpu1/online
         echo 1 > /sys/devices/system/cpu/cpu2/online
         echo 1 > /sys/devices/system/cpu/cpu3/online
