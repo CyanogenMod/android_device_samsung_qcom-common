ba0443f53552dfbfc00ec8bc8f5f36685caa0a73
philz-cwm6 <phytowardt@gmail.com>
2014-10-15 14:55:18 -0700

    qcom-common: Copy recovery scripts to correct place
    Change-Id: I759dd38d36b687c9aa86616cdce1e4b1a7025071

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -5,5 +5,13 @@ LOCAL_MODULE       := init.recovery.qcom.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.recovery.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := postrecoveryboot.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/postrecoveryboot.sh
+LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)/sbin
 include $(BUILD_PREBUILT)
