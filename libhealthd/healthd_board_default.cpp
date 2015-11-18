fa263f17ef8722e06f64435425a7890982da1fb8
Fenglin Wu <fenglinw@codeaurora.org>
2015-11-18 05:23:45 +0100

    healthd: Draw battery level
    Change-Id: Ifd6312e0900c999facb8c924ff41edd11fdd94dc

--- libhealthd/healthd_board_default.cpp
+++ libhealthd/healthd_board_default.cpp
@@ -16,6 +16,7 @@
  */
 
 #include <healthd.h>
+#include "minui/minui.h"
 
 void healthd_board_init(struct healthd_config *config)
 {
@@ -28,16 +29,29 @@ void healthd_board_init(struct healthd_config *config)
     config->batteryTechnologyPath  = "/sys/class/power_supply/battery/technology";
 }
 
+#define STR_LEN 8
+void healthd_board_mode_charger_draw_battery(
+                struct android::BatteryProperties *batt_prop)
+{
+    char cap_str[STR_LEN];
+    int x, y;
+    int str_len_px;
+    static int char_height = -1, char_width = -1;
+
+    if (char_height == -1 && char_width == -1)
+        gr_font_size(&char_width, &char_height);
+    snprintf(cap_str, (STR_LEN - 1), "%d%%", batt_prop->batteryLevel);
+    str_len_px = gr_measure(cap_str);
+    x = (gr_fb_width() - str_len_px) / 2;
+    y = (gr_fb_height() + char_height) / 2;
+    gr_color(0xa4, 0xc6, 0x39, 255);
+    gr_text(x, y, cap_str, 0);
+}
 
 int healthd_board_battery_update(__attribute__((unused)) struct android::BatteryProperties *props)
 {
     // return 0 to log periodic polled battery status to kernel log
     return 1;
-}
-
-void healthd_board_mode_charger_draw_battery(struct android::BatteryProperties*)
-{
-
 }
 
 void healthd_board_mode_charger_battery_update(struct android::BatteryProperties*)
