f2cb19627612ab6b078dcc786831b989b5b9143e
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-08-08 16:33:38 +0800

    qcom: Remove healthd extension and charger images
    According to the original commit, the healthd extension was
    needed to get the correct battery capacity on d2 and jf.
    
    However, along the way, it seems that the custom healthd is
    no longer needed, is no longer used on jf.
    
    Furthermore, CM now has its own healthd extension and charger
    images, which work just fine on jf.
    
    Hence, remove the obsolete healthd extension and charger images.
    
    Change-Id: Idfa80434075818fb02eff8ba1ac33556608ed636

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -31,9 +31,6 @@ USE_OPENGL_RENDERER := true
 TARGET_USES_C2D_COMPOSITION := true
 TARGET_USES_ION := true
 
-# Healthd
-BOARD_HAL_STATIC_LIBRARIES := libhealthd.msm
-
 # Qualcomm support
 BOARD_USES_QCOM_HARDWARE := true
 
